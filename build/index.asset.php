4c3418dcf482a9e2b893c41ed0ef26ebb1916aa5
hjude-design <harrison@hjude.com>
2024-07-07 07:44:47 -0400

    added block supports

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fb1dc972e551c9e6cd7e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c6240ec5abf435604c3c');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'bffc1e967b5713730767');
+<?php return array('dependencies' => array(), 'version' => '46622d5093ab86a9153d');
