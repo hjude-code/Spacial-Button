78a80c4fe59751b3480fc82f111b34a25b335c6d
hjude-design <harrison@hjude.com>
2024-07-07 14:14:48 -0400

    added border color attribute

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '880ed5972929a0b37f7a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8552d661415b4e0e531a');
