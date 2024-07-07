340e24ca6fbc635333ec64e281178941edc5ffee
hjude-design <harrison@hjude.com>
2024-07-07 12:46:49 -0400

    changed icon to paperclip

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e0ca15ab6bf02c88a922');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '880ed5972929a0b37f7a');
