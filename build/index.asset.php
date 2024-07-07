25583e8cd1a2cc2c6259695095852b81cd2bb05a
hjude-design <harrison@hjude.com>
2024-07-07 10:34:24 -0400

    added pointer events back to inner container within the block editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'eb80a123d55217f11b46');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dcaf1314a8a0456ad9ca');
