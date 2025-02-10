6c3f7c17d42d34ee6852b8c0b8128de7497f1e32
hjude-design <harrison@hjude.com>
2025-02-10 13:51:19 -0500

    added responsive view

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '159084ee5cceda3ee4a0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bdcf11bc2e91811cf1b9');
